b9672e16d8010c2295a3985bec4bc771b68cbd29
Axaelon <87068399+Axaelon@users.noreply.github.com>
2021-08-21 21:53:16 +0200

    initial commit

---
+++ AlternityRP.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+const prefix = "?";
+
+Client.on("ready", () => {
+    console.log("Bot opérationnel");
+});
+
+Client.on("messageCreate", message => {
+    if (message.author.bot) return;
+
+    
+});
+
+Client.login(process.env.TOKEN);
